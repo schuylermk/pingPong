1a04a6b383320091be5ccbf77525fc16119b52f9
schuylermk <schuyler.m.k@gmail.com>
2016-01-26 20:29:34 -0800

    Create and pass spec for numbers divisible by 15

--- spec/specs.js
+++ spec/specs.js
@@ -5,18 +5,9 @@ describe('pingLoop', function() {
   it("will change any multiple of 3 to ping", function() {
     expect(pingLoop(3)).to.eql([1,2,"ping"]);
   });
-  it("will change any multiple of 5 to pong", function () {
+  it("will change any multiple of 5 to pong", function() {
     expect(pingLoop(5)).to.eql([1,2,"ping",4,"pong"]);
   });
+  it("will change multiples of 15 to pingpong", function() {   expect(pingLoop(15)).to.eql([1,2,"ping",4,"pong","ping",7,8,"ping","pong",11,"ping",13,14,"pingpong"]);
+  });
 });
-
-
-
-  //   expect(pingLoop(5)).to.eql([1,2,3,4,5]);
-  // });
-  // it("will change multiples of 15 to pingpong", function () {
-  //   expect(pingLoop(15)).to.eql([1,2,"ping",4,"pong","ping",7,8,"ping","pong",11,"ping",13,14,"pingpong"]);
-  // });
-
-
-  // });
