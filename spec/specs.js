bcb6a2e6b5757d5529810cb8f1bf0a7eb706feeb
schuylermk <schuyler.m.k@gmail.com>
2016-01-26 20:26:54 -0800

    Create and pass spec for numbers 3 and 5

--- spec/specs.js
+++ spec/specs.js
@@ -2,6 +2,12 @@ describe('pingLoop', function() {
   it("will count/increment up from one, by one, until reaching user inputted number", function() {
     expect(pingLoop(2)).to.eql([1,2]);
   });
+  it("will change any multiple of 3 to ping", function() {
+    expect(pingLoop(3)).to.eql([1,2,"ping"]);
+  });
+  it("will change any multiple of 5 to pong", function () {
+    expect(pingLoop(5)).to.eql([1,2,"ping",4,"pong"]);
+  });
 });
 
 
@@ -11,10 +17,6 @@ describe('pingLoop', function() {
   // it("will change multiples of 15 to pingpong", function () {
   //   expect(pingLoop(15)).to.eql([1,2,"ping",4,"pong","ping",7,8,"ping","pong",11,"ping",13,14,"pingpong"]);
   // });
-  // it("will change any multiple of 5 to pong", function () {
-  //   expect(pingLoop(5)).to.eql(["pong"]);
-  // });
-  // it("will change any multiple of 3 to ping", function () {
-  //   expect(pingLoop(3)).to.eql(["ping"]);
-  // });
+
+
   // });
