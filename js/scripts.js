845915ce25595c4e6ab92ca33e13999579c1609f
schuylermk <schuyler.m.k@gmail.com>
2016-01-28 18:26:25 -0800

    Adding pseudo code for jQuery to append pings, pongs, numbers to separate columns in the html

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,7 @@ var pingLoop = function(number) {
   for (var i = 1; i <= number; i += 1) {
     if (i % 15 === 0) {
       numberBox.push("pingpong");
+
     } else if (i % 5 === 0) {
       numberBox.push("pong");
     } else if (i % 3 === 0) {
@@ -12,12 +13,25 @@ var pingLoop = function(number) {
     }
   } return numberBox;
 };
-// $(document).ready(function(){
-//   $("form").submit(function(event) {
-//   var userEntry = $("input#userInput").val();
-//   var result = pingLoop(userEntry);
-//     $(".output").text(result);
-//
-//     event.preventDefault();
-//   });
-// });
+$(document).ready(function(){
+  $("form").submit(function(event) {
+  var userEntry = $("input#userInput").val();
+
+  var result = pingLoop(userEntry);
+
+    $(".output").text(result);
+
+    event.preventDefault();
+  });
+});
+
+
+if (ping) {
+  append to ul#ping;
+} else if (pong) {
+  append to ul#pong;
+} else if (pingpong) {
+  append to ul#pingpong;
+} else {
+  append to ul#plainNumbers ;
+}
